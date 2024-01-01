1fef90173e0fd34d8c2b11822370bc8e72cd1b6a
sobot <yoonseok.bae98@gmail.com>
2024-01-01 22:52:25 +0900

    Add AnimalMemberVo class

---
+++ src/main/java/com/soboti/animalapi/vo/AnimalMemberVo.java
@@ -0,0 +1,10 @@
+package com.soboti.animalapi.vo;
+
+import lombok.Data;
+
+@Data
+public class AnimalMemberVo {
+    private Integer member_no;
+    private String member_en;
+    private String member_ko;
+}
